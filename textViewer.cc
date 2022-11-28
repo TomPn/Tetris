248af5d6fff898f057677ee7be245db641420545
Tom Pan <tommmpn@gmail.com>
2022-11-28 00:05:24 -0500

    Update implementations

--- textViewer.cc
+++ textViewer.cc
@@ -1,8 +1,9 @@
 #include <iostream>
 #include "textViewer.h"
 using std::cout;
+using std::endl;
 
-TextViewer::TextViewer(Game *subject): subject{subject}, top{0}, bot{24}, left{0}, right{28} {
+TextViewer::TextViewer(Game *subject): subject{subject}, top{0}, bot{25}, left{0}, right{28} {
     subject->attach(this);
 }
 
@@ -11,17 +12,47 @@ TextViewer::~TextViewer() {
 }
 
 void TextViewer::notify() {
-//   out << '+';
-//   for (int j = left; j <= right; ++j) out << '-';
-//   out << '+' << std::endl;
-//   for (int i = top; i <= bot; ++i) {
-//     out << '|';
-//     for (int j = left; j <= right; ++j) {
-//       out << subject->getState(i, j);
-//     }
-//     out << '|' << std::endl;
-//   }
-//   out << '+';
-//   for (int j = left; j <= right; ++j) out << '-';
-//   out << '+' << std::endl;
+    for (int row = top; row < bot; ++row) {
+        // print the first line of the game
+        if (row == 0) {
+            for (int i = 0; i < 2; ++i) {
+                // each player's level
+                out << "Level:";
+                for (int j = 0; j < 4; ++j) out << ' ';
+                out << subject->getLevel(i);
+                // print the space between the first player and the second player
+                if (i == 0) {
+                    for (int j = 0; j < 6; ++j) out << ' ';
+                }
+            }
+            out << endl;
+        } else if (row == 1) {
+            for (int i = 0; i < 2; ++i) {
+                // each player's level
+                out << "Score:";
+                for (int j = 0; j < 4; ++j) out << ' ';
+                out << subject->getScore(i);
+                // print the space between the first player and the second player
+                if (i == 0) {
+                    for (int j = 0; j < 6; ++j) out << ' ';
+                }
+            }
+            out << endl;
+        } else if (row == 2 || row == 21) {
+            // print the breakline between the statistics and the actual game
+            for (int col = left; col < right; ++col) out << '-';
+            out << endl;
+        } else if (row == 22) {
+            // print the Next section for both players
+            out << "Next:";
+            for (int j = 0; j < 10; ++j) out << ' ';
+            out << "Next:" << endl;
+        } else {
+            // print the actual games
+            for (int col = left; col < right; ++col) {
+                out << subject->getState(row, col);
+            }
+            out << endl;
+        }
+    }
 }
