cbafdf4b782b151ac9ba8be52a3ed0adc8900736
tinaafang <94018300+tinaafang@users.noreply.github.com>
2022-11-27 22:31:34 -0500

    update

--- iBlock.h
+++ iBlock.h
@@ -1,15 +1,12 @@
 #ifndef IBLOCK_H
-#define IBLOCK_H 
+#define IBLOCK_H
 #include "block.h"
 
-class IBlock : public Block {
-    public:
-        IBlock(Cell *cell1, Cell *cell2, Cell *cell3, Cell *cell4, int alive): Block(cell1, cell2, cell3, cell4, alive) {};
-        void rotate(bool clockwise);
+class IBlock : public Block
+{
+public:
+    IBlock(Cell *cell1, Cell *cell2, Cell *cell3, Cell *cell4, int alive, int level) : Block(cell1, cell2, cell3, cell4, alive, level){};
+    void rotate(bool clockwise);
 };
 
-
 #endif
-
-
-
--- level.cc
+++ level.cc
@@ -1,129 +1,146 @@
 #include "level.h"
+#include "block.h"
+#include "cell.h"
 #include <string>
 #include <fstream>
+#include <vector>
 
 // readFile reads the strings in file/sequence/scriptfile store them in a vector of string
-std::vector<std::string> Level::readFile(std::string fileName){
+std::vector<std::string> Level::readFile(std::string fileName)
+{
     // initialize the file object, a string, and a vector of string
-    ifstream f{sequence1};
+    std::ifstream f{fileName};
     std::string s;
     std::vector<std::string> content;
     // read each string in file and insert them into the vecter of string
-    while(f << s){
+    while (f >> s)
+    {
         content.emplace_back(s);
     }
     return content;
 }
 
-Level(std::vector<std::vector<Cell *>> cells):file{""},sequence{""},scriptfile{""},randomBool{false},scriptfileBool{false},seedBool{false},seed{0}, cells{cells}, sequenceIndex{0},scriptfileIndex{0},fileIndex{0} {
+Level::Level(std::vector<std::vector<Cell *>> cells) : file{""}, sequence{""}, scriptfile{""}, randomBool{false},
+                                                       scriptfileBool{false}, seedBool{false}, seed{0}, cells{cells}, sequenceIndex{0}, scriptfileIndex{0}, fileIndex{0}
+{
     // initialize a placeholder for the vector of string
     std::vector<std::string> placeholder{""};
     // if fileName isn't empty, call readFile to read the strings in the file and store them in a vector of string
-    if(file != ""){
+    if (file != "")
+    {
         fileContent = Level::readFile(file);
-    }else{
+    }
+    else
+    {
         fileContent = placeholder;
     }
 
-    if(sequence != ""){
+    if (sequence != "")
+    {
         sequenceContent = Level::readFile(sequence);
-    }else{
+    }
+    else
+    {
         sequenceContent = placeholder;
     }
 
-    if(scriptfile != ""){
+    if (scriptfile != "")
+    {
         scriptfileContent = Level::readFile(scriptfile);
-    }else{
+    }
+    else
+    {
         scriptfileContent = placeholder;
     }
-
 }
 
-Block * Level::CreateIBlock(int level){
-    string blockStr = "I";
-    cells[19][0].setChar(blockStr);
-    cells[19][1].setChar(blockStr);
-    cells[19][2].setChar(blockStr);
-    cells[19][3].setChar(blockStr);
-    Block * nextBlock = new Block{cells[19][0],cells[19][1],cells[19][2],cells[19][3],4,level};
+Block *Level::CreateIBlock(int level)
+{
+    char blockChar = 'I';
+    cells[19][0]->setChar(blockChar);
+    cells[19][1]->setChar(blockChar);
+    cells[19][2]->setChar(blockChar);
+    cells[19][3]->setChar(blockChar);
+    Block *nextBlock = new Block{cells[19][0], cells[19][1], cells[19][2], cells[19][3], 4, level};
     return nextBlock;
 }
-Block * Level::CreateJBlock(int level){
-    string blockStr = "J";
+Block *Level::CreateJBlock(int level)
+{
+    std::string blockStr = "J";
     cells[18][0].setChar(blockStr);
     cells[19][0].setChar(blockStr);
     cells[19][1].setChar(blockStr);
     cells[19][2].setChar(blockStr);
-    Block * nextBlock = new Block{cells[18][0],cells[19][0],cells[19][1],cells[19][2],4,level};
+    Block *nextBlock = new Block{cells[18][0], cells[19][0], cells[19][1], cells[19][2], 4, level};
     return nextBlock;
 }
-Block * Level::CreateLBlock(int level){
+Block *Level::CreateLBlock(int level)
+{
     string blockStr = "L";
     cells[18][2].setChar(blockStr);
     cells[19][0].setChar(blockStr);
     cells[19][1].setChar(blockStr);
     cells[19][2].setChar(blockStr);
-    Block * nextBlock = new Block{cells[18][2],cells[19][0],cells[19][1],cells[19][2],4,level};
+    Block *nextBlock = new Block{cells[18][2], cells[19][0], cells[19][1], cells[19][2], 4, level};
     return nextBlock;
 }
-Block * Level::CreateOBlock(int level){
+Block *Level::CreateOBlock(int level)
+{
     string blockStr = "O";
     cells[18][0].setChar(blockStr);
     cells[18][1].setChar(blockStr);
     cells[19][0].setChar(blockStr);
     cells[19][1].setChar(blockStr);
-    Block * nextBlock = new Block{cells[18][0],cells[18][1],cells[19][0],cells[19][1],4,level};
+    Block *nextBlock = new Block{cells[18][0], cells[18][1], cells[19][0], cells[19][1], 4, level};
     return nextBlock;
 }
-Block * Level::CreateSBlock(int level){
+Block *Level::CreateSBlock(int level)
+{
     string blockStr = "S";
     cells[18][1].setChar(blockStr);
     cells[18][2].setChar(blockStr);
     cells[19][0].setChar(blockStr);
     cells[19][1].setChar(blockStr);
-    Block * nextBlock = new Block{cells[18][1],cells[18][2],cells[19][0],cells[19][1],4,level};
+    Block *nextBlock = new Block{cells[18][1], cells[18][2], cells[19][0], cells[19][1], 4, level};
     return nextBlock;
 }
-Block * Level::CreateZBlock(int level){
+Block *Level::CreateZBlock(int level)
+{
     string blockStr = "Z";
     cells[18][0].setChar(blockStr);
     cells[18][1].setChar(blockStr);
     cells[19][1].setChar(blockStr);
     cells[19][2].setChar(blockStr);
-    Block * nextBlock = new Block{cells[18][0],cells[18][1],cells[19][1],cells[19][2],4,level};
+    Block *nextBlock = new Block{cells[18][0], cells[18][1], cells[19][1], cells[19][2], 4, level};
     return nextBlock;
 }
-Block * Level::CreateTBlock(int level){
+Block *Level::CreateTBlock(int level)
+{
     string blockStr = "T";
     cells[18][0].setChar(blockStr);
     cells[18][1].setChar(blockStr);
     cells[18][2].setChar(blockStr);
     cells[19][1].setChar(blockStr);
-    Block * nextBlock = new Block{cells[18][0],cells[18][1],cells[18][2],cells[19][1],4,level};
+    Block *nextBlock = new Block{cells[18][0], cells[18][1], cells[18][2], cells[19][1], 4, level};
     return nextBlock;
 }
 
-
-
-
-
-
-
-
 // mutate scriptfileBool and scriptfile
-void Level::setScriptfile(bool scriptfileBool,std::string scriptfile = ""){
+void Level::setScriptfile(bool scriptfileBool, std::string scriptfile = "")
+{
     this->scriptfileBool = scriptfileBool;
     this->scriptfile = scriptfile;
 }
 
 // mutate seedBool and seed
-void Level::setSeed(bool seedBool, unsigned int seed){
+void Level::setSeed(bool seedBool, unsigned int seed)
+{
     this->seedBool = seedBool;
     this->seed = seed;
 }
 
-
 // mutate randomBool
-void Level::setRandom(bool random){
+void Level::setRandom(bool random)
+{
     randomBool = random;
 }
